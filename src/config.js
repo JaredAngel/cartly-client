81fd8eacbc9d87f487521462de2f1a6b51a7b9d7
Jared Angel <jared.aescobedo@gmail.com>
2021-01-31 15:37:21 -0600

    deployment prep

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  REACT_APP_API_BASE: 'https://cartly-001.herokuapp.com/api' || 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://cartly-001.herokuapp.com/api',
   TOKEN_KEY: 'cartly-client-auth-token',
 };
